0b0f6eeeeb3d54e4987eba80826fd89b09eca6e3
HarryWang <zuhanwang@outlook.com>
2025-09-21 16:41:16 -0400

    created 8 bit counter

--- src/project.v
+++ src/project.v
@@ -17,9 +17,21 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  // assign uio_out = 0;
+  // assign uio_oe  = 0;
+
+  wire [7:0] counter_out;
+  counter counter_inst1(
+    .in_load_value(uio_in),
+    .in_load_now(ui_in[0]),
+    .in_clk = clk,
+    .in_rst_n = rst_n,
+    .out_counter_value(uio_out),
+    .out_ena(uio_oe)
+  );
+
+  assign uo_out  = 0;
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
