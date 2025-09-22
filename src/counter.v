e7c5b1527dacbb820e6b719e3bda8f3ae4db00ad
HarryWang <zuhanwang@outlook.com>
2025-09-22 10:17:30 -0400

    changed assignment conditions

--- src/counter.v
+++ src/counter.v
@@ -16,14 +16,16 @@ module counter(
 always@(posedge in_clk or negedge in_rst_n) begin
     if(!in_rst_n) begin
         out_counter_value <= 0;
+        out_ena <= 8'h0;
     end
     else if(in_load_now) begin //have to hold in_load_now for at least 2 clock cycles
         if(!out_ena) out_counter_value <= in_load_value;
+        out_ena <= {8{in_write_now & ~in_load_now}};
     end
     else begin
         out_counter_value <= out_counter_value + 1;
+        out_ena <= {8{in_write_now & ~in_load_now}};
     end
-    out_ena <= {8{in_write_now & ~in_load_now}};
 end
 
 
